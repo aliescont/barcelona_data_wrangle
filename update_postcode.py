e1246403b266137c72cf9405d67986a0e6082601
aliescont <aescontrela@gmail.com>
2017-09-24 13:20:27 +0200

    Add files via upload

---
+++ update_postcode.py
@@ -0,0 +1,41 @@
+import re
+
+
+test = ['08012','28038', '08012 Barcelona', '28239']
+
+def update_postcode(postcode):
+    # new regular expression pattern
+    search = re.match(r'^\d*(\d{5}).*', postcode)
+    # select the group that is captured
+    expected = re.match (r'^[0][8]', postcode)
+    if expected is None:
+        clean_postcode = 'NA'
+    else:
+         clean_postcode = search.group(1)
+    return clean_postcode  
+
+for item in test:
+    cleaned = update_postcode(item)
+    print cleaned
+
+
+test = ['08012','28038', '08012 Barcelona', '28239']
+def update_postcode(postcode):
+    expected = re.match (r'^[0][8]', postcode)
+    if expected is None:
+        clean_postcode = 'NA'
+    else:
+         # new regular expression pattern
+        search = re.match(r'^\d*(\d{5}).*', postcode)
+         # select the group that is captured
+        clean_postcode = search.group(1)
+    
+    return clean_postcode  
+
+for item in test:
+    cleaned = update_postcode(item)
+    print cleaned
+
+
+
+
